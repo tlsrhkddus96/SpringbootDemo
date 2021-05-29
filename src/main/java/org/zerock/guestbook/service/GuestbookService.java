b6750a72c79a8372193155a5cfac43d67c939e5b
tlsrhkddus96 <tlsrhkddus96@users.noreply.github.com>
2021-05-29 20:16:03 +0900

    2021/05/29 Guestbook CRUD 검색,페이징

--- src/main/java/org/zerock/guestbook/service/GuestbookService.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookService.java
@@ -9,6 +9,12 @@ public interface GuestbookService {
 
     Long register(GuestbookDTO dto);
 
+    GuestbookDTO read(Long gno);
+
+    void remove(Long gno);
+
+    void modify(GuestbookDTO dto);
+
     PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
 
     default Guestbook dtoToEntity(GuestbookDTO dto){
